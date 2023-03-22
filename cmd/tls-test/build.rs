ab5184ca4befb8059ab95c92ca9c417c8dd4f085
hillium <yujuncen@pingcap.com>
2023-03-22 17:17:41 +0800

    make it better
    Signed-off-by: hillium <yujuncen@pingcap.com>

--- cmd/tls-test/build.rs
+++ cmd/tls-test/build.rs
@@ -1,1 +1,77 @@
-include!("../build.rs");
+// Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
+
+use std::process::Command;
+
+// Copy and paste from ../build.rs.
+fn link_cpp(tool: &cc::Tool) {
+    let stdlib = if tool.is_like_gnu() {
+        "libstdc++.a"
+    } else if tool.is_like_clang() {
+        "libc++.a"
+    } else {
+        // Don't link to c++ statically on windows.
+        return;
+    };
+    link_sys_lib(stdlib, tool)
+}
+
+fn link_sys_lib(lib: &str, tool: &cc::Tool) {
+    let output = tool
+        .to_command()
+        .arg("--print-file-name")
+        .arg(lib)
+        .output()
+        .unwrap();
+    if !output.status.success() || output.stdout.is_empty() {
+        // fallback to dynamically
+        return;
+    }
+    let path = match std::str::from_utf8(&output.stdout) {
+        Ok(path) => std::path::PathBuf::from(path),
+        Err(_) => return,
+    };
+    if !path.is_absolute() {
+        return;
+    }
+    // remove lib prefix and .a postfix.
+    let libname = &lib[3..lib.len() - 2];
+    // Get around the issue "the linking modifiers `+bundle` and `+whole-archive`
+    // are not compatible with each other when generating rlibs"
+    println!(
+        "cargo:rustc-link-lib=static:-bundle,+whole-archive={}",
+        &libname
+    );
+    println!(
+        "cargo:rustc-link-search=native={}",
+        path.parent().unwrap().display()
+    );
+}
+
+fn git(what: &str, subcmd: &[&str]) {
+    let git_hash = Command::new("git")
+        .args(subcmd)
+        .output()
+        .ok()
+        .and_then(|out| String::from_utf8(out.stdout).ok());
+    if let Some(hash) = git_hash {
+        println!(
+            "cargo:rustc-env=TIKV_BUILD_{}={}",
+            what.to_uppercase(),
+            hash
+        );
+    }
+}
+
+fn main() {
+    println!(
+        "cargo:rustc-env=TIKV_BUILD_TIME={}",
+        time::now_utc().strftime("%Y-%m-%d %H:%M:%S").unwrap()
+    );
+
+    let tool = cc::Build::default().get_compiler();
+    link_cpp(&tool);
+    // Maybe we can link more sys libraries.
+    // END copy & paste from ../build.rs.
+    git("git_hash", &["rev-parse", "HEAD"]);
+    git("git_branch", &["rev-parse", "--abbrev-ref", "HEAD"]);
+}
--- cmd/tls-test/src/main.rs
+++ cmd/tls-test/src/main.rs
@@ -1,61 +1,114 @@
 use std::{sync::Arc, time::Duration};
 
-use backup_stream::metadata::{
-    keys::{KeyValue, MetaKey},
-    store::MetaStore,
-    ConnectionConfig, LazyEtcdClient,
+use backup_stream::{
+    errors::{Error, Result},
+    metadata::{
+        keys::MetaKey,
+        store::{Keys, MetaStore, Snapshot},
+        ConnectionConfig, LazyEtcdClient,
+    },
 };
 use clap::Parser;
-use rand::Rng;
 use security::{SecurityConfig, SecurityManager};
+use server::setup::initial_logger;
+use slog_global::info;
+use tikv::config::TikvConfig;
 
 #[derive(Clone, Debug, PartialEq, Default, Parser)]
 struct Config {
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "https://127.0.0.1:2379")]
     endpoints: Vec<String>,
-    #[arg(long)]
+    #[arg(long, default_value = "")]
+    tikv_config_path: String,
+    #[arg(long, default_value = "")]
     ca_path: String,
-    #[arg(long)]
+    #[arg(long, default_value = "")]
     cert_path: String,
-    #[arg(long)]
+    #[arg(long, default_value = "")]
     key_path: String,
 }
 
-impl From<Config> for SecurityConfig {
-    fn from(c: Config) -> Self {
-        Self {
+struct Run {
+    security_manager: Arc<SecurityManager>,
+    endpoints: Vec<String>,
+}
+
+type MayFail<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+impl Config {
+    fn convert_direct(self) -> MayFail<Run> {
+        let c = self;
+        let sec = SecurityConfig {
             ca_path: c.ca_path,
             cert_path: c.cert_path,
             key_path: c.key_path,
-            ..Self::default()
-        }
+            ..SecurityConfig::default()
+        };
+        let endpoints = c.endpoints;
+        let security_manager = Arc::new(SecurityManager::new(&sec)?);
+        Ok(Run {
+            endpoints,
+            security_manager,
+        })
     }
 }
 
-fn main() {
+fn init_log() {
+    initial_logger(&Default::default());
+}
+
+async fn execute_test<M: MetaStore>(cli: &M) -> Result<()> {
+    let snap = cli.snapshot().await?;
+    let query = Keys::Prefix(MetaKey(b"/tidb/br-stream".to_vec()));
+    let keys = snap.get(query).await?;
+    info!("Query successed!"; "pitr-key-len" => %keys.len());
+    Ok(())
+}
+
+fn init_run(cfg: &Config) -> MayFail<Run> {
+    if [&cfg.ca_path, &cfg.cert_path, &cfg.key_path]
+        .iter()
+        .all(|x| !x.is_empty())
+    {
+        return Ok(cfg.clone().convert_direct()?);
+    }
+    if !cfg.tikv_config_path.is_empty() {
+        let content = std::fs::read(&cfg.tikv_config_path)?;
+        let kvcfg: TikvConfig = toml::from_str(&String::from_utf8(content)?)?;
+        info!("Loaded config from TiKV config."; "cfg" => ?kvcfg.security, "pds" => ?kvcfg.pd.endpoints);
+        let security_manager = Arc::new(SecurityManager::new(&kvcfg.security)?);
+        // If manually configuried the endpoints, don't override it by the TiKV config.
+        let endpoints = if cfg.endpoints != [ "https://127.0.0.1:2379"] { cfg.endpoints.clone() } else { kvcfg.pd.endpoints };
+        return Ok(Run {
+            endpoints,
+            security_manager,
+        });
+    }
+    Err("must provide [--ca-path, --cert-path, --key-path] or --tikv-config-path".into())
+}
+
+fn main() -> Result<()> {
     let cfg = Config::parse();
-    let security_mgr = Arc::new(
-        SecurityManager::new(&cfg.clone().into()).expect("failed to create security manager."),
+    init_log();
+    info!("Welcome to TLS compatibility test utility!";
+        "build_hash" => %option_env!("TIKV_BUILD_GIT_HASH").unwrap_or("<UNKNOWN>"),
+        "build_branch" => %option_env!("TIKV_BUILD_GIT_BRANCH").unwrap_or("<UNKNOWN>")
     );
-    println!("using config = {:?}", cfg);
+    info!("Using config."; "cfg" => ?cfg);
+    let run = init_run(&cfg).map_err(|err| Error::Other(format!("{}", err).into()))?;
+
     let ccfg = ConnectionConfig {
         keep_alive_interval: Duration::from_secs(3),
         keep_alive_timeout: Duration::from_secs(10),
-        tls: security_mgr.tonic_tls_config(),
+        tls: run.security_manager,
     };
-    println!("using connection config = {:?}", ccfg);
-    let etcd_cli = LazyEtcdClient::new(cfg.endpoints.as_slice(), ccfg);
-    let client_id = rand::thread_rng().gen::<u64>();
-    println!("using client_id = {}", client_id);
+    let etcd_cli = LazyEtcdClient::new(run.endpoints.as_slice(), ccfg);
+
     let t = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap();
-    let kv = KeyValue(
-        MetaKey("/hello/world".as_bytes().to_vec()),
-        client_id.to_string().into_bytes(),
-    );
-    println!("setting {:?} to PD", kv);
-    t.block_on(etcd_cli.set(kv)).unwrap();
-    println!("success!");
+    t.block_on(execute_test(&etcd_cli))?;
+    info!("SUCCESS");
+    Ok(())
 }
