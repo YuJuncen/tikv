f632bb0fe37c852cf77dbb7c1c9b25a6371294c7
Yu Juncen <yu745514916@live.com>
2024-01-23 16:12:28 +0800

    first version
    Signed-off-by: Yu Juncen <yu745514916@live.com>

--- cmd/kylin-disk-test/src/main.rs
+++ cmd/kylin-disk-test/src/main.rs
@@ -1,9 +1,15 @@
-use std::path::Path;
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Seek, SeekFrom},
+    os::unix::fs::OpenOptionsExt,
+    path::Path,
+};
+
+const CHUNK_SIZE: usize = 4096;
 
 fn put_random_file(path: &Path, size: usize) -> std::io::Result<()> {
     let mut rng = rand::thread_rng();
     let mut file = std::fs::File::create(path)?;
-    const CHUNK_SIZE: usize = 4096;
     let mut rem = size;
     let mut buf = vec![0u8; CHUNK_SIZE];
     while rem > 0 {
@@ -16,10 +22,46 @@ fn put_random_file(path: &Path, size: usize) -> std::io::Result<()> {
     Ok(())
 }
 
-fn read_random(path: &Path) {
+#[repr(C, align(4096))]
+#[alignment(CHUNK_SIZE)]
+struct DirectBuffer([u8; CHUNK_SIZE * 16]);
 
+fn read_chunk(fd: &mut File, offset: usize, length: usize) -> std::io::Result<()> {
+    let mut buf = DirectBuffer([0u8; CHUNK_SIZE * 16]);
+    fd.seek(SeekFrom::Start(offset))?;
+    let mut rem = length;
+    while rem > 0 {
+        let buf = &mut buf[..(rem.min(buf.0.len()))];
+        let n = fd.read(buf)?;
+        rem -= n;
+    }
+    Ok(())
 }
 
-fn main() {
-    let (_, read) = backup_stream::utils::with_record_read_throughput(f)
+fn align_to_4096(num: usize) -> usize {
+    n - (n % 4096)
+}
+
+fn main() -> std::io::Result<()> {
+    let tmp = tempfile::tempdir()?;
+    let file_path = tmp.path().join("random.bin");
+    put_random_file(&file_path, 1 * 1024 * 1024 * 1024)?;
+    // Open the file with direct I/O.
+    let mut fd = OpenOptions::new()
+        .custom_flags(0x4000 /* O_DIRECT */ | libc::O_RDONLY)
+        .open(&file_path)?;
+    for _ in 0..100 {
+        let offset = align_to_4096(rng.gen_range(0..(1 * 1024 * 1024 * 1024)));
+        let length = align_to_4096(rng.gen_range(0..(CHUNK_SIZE * 32)));
+        let (res, size) = backup_stream::utils::with_record_read_throughput(|| {
+            read_chunk(&mut fd, offset, length)
+        });
+        res?;
+        if length != size {
+            println!("Diff {} vs {}", length, size);
+        } else {
+            print!(".");
+        }
+    }
+    Ok(())
 }
